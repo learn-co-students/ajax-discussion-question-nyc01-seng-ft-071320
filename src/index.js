0ac6639f068ba944596dea8c91ac5f2e4c14f278
junko911 <junkotahara911@gmail.com>
2020-08-28 15:43:02 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,45 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+  const button = document.querySelector('.btn')
+  const fullname = document.querySelector('#fullname')
+  const email = document.querySelector('#email')
+  const street = document.querySelector('#street')
+  const city = document.querySelector('#city')
+  const state = document.querySelector('#state')
+  const postcode = document.querySelector('#postcode')
+  const phone = document.querySelector('#phone')
+  const cell = document.querySelector('#cell')
+  const dateOfBirth = document.querySelector('#date_of_birth')
+  const profilePicture = document.querySelector('#profile_picture')
+
+  const getPerson = () => {
+    fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(person => {
+        const result = person.results[0]
+
+        fullname.textContent = `${result.name.first} ${result.name.last}`
+        email.textContent = result.email
+        street.textContent = `${result.location.street.number}, ${result.location.street.name}`
+        city.textContent = result.location.city
+        state.textContent = result.location.state
+        postcode.textContent = result.location.postcode
+        phone.textContent = result.phone
+        cell.textContent = result.cell
+        dateOfBirth.textContent = result.dob.date
+        profilePicture.src = result.picture.medium
+      })
+  }
+
+  const clickHandler = () => {
+    document.addEventListener('click', e => {
+      if (e.target === button ) {
+        getPerson()
+      }
+    })
+  }
+
+
+  clickHandler()
 });
